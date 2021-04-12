b3266e3cfcdab79cec8d09761f7b68ad5a54744c
michalml <michalml@P-MICHALML>
2021-04-12 19:33:00 +0300

    initial commit

---
+++ src/Product/Product.js
@@ -0,0 +1,15 @@
+
+import './Product.css';
+
+const Product = (details) => 
+	  <div className="product-card">
+        <div className="product-image">
+          <img src={details.image} />
+        </div>
+        <div className="product-info">
+          <h5>{details.title}</h5>
+          <h6>${details.price.toString()}</h6>
+        </div>
+    </div>;
+
+export default Product;
---
+++ src/Select/Select.js
@@ -0,0 +1,14 @@
+
+// import './Select.css';
+
+// const Select  = ({title, list}) => 
+// 	<div className="collection-sort">
+// 		<label>{title}:</label>
+// 		<select>
+// 			{
+// 				list.map(val => <option value="/">{val}</option>)
+// 			}
+// 		</select>
+// 	</div>;
+
+// export default Select;
