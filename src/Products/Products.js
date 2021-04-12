0e07ff099a96b882dfb9582ed4e010a7317d9cdf
michalml <michalml@P-MICHALML>
2021-04-12 20:13:15 +0300

    initial commit

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -2,8 +2,6 @@
 import Product from '../Product/Product.js';
 import './Products.css';
 
-
-
 const Products = ({products}) =>
 	<section className="products">
 		{products.map(p => <Product key={id++} title={p.title} image={p.image} price={p.price}></Product>)}			
