3b4d03ee170a64b0fe3c745ed9cccd8e66a475ab
michalml <michalml@P-MICHALML>
2021-04-12 20:06:39 +0300

    initial commit

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -2,13 +2,6 @@
 import Product from '../Product/Product.js';
 import './Products.css';
 
-const product = {
-	title: "Winter Jacket",
-	image: "https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369",
-	price: 99.99
-};
-
-let id = 1;
 
 
 const Products = ({products}) =>
