0c53206acb34a5b65e4a6a8c02ff34491c00da11
michalmalik27 <michalush17@gmail.com>
2021-04-18 13:36:34 +0300

    remove empty rowst

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -2,13 +2,9 @@
 import Product from '../Product/Product.js';
 import './Products.css';
 
-
 const Products = ({products}) =>
-
 	<section className="products">
 		{products.map(p => <Product key={p.id} title={p.title} image={p.image} price={p.price}></Product>)}	
-
 	</section>;
-
 
 export default Products;
